363d7ba82cd8ebbaa87543ae7b83b42c0dae054f
shaggyrogers <shaggyrogers@not.real.net>
2025-05-15 15:01:19 +1000

    windowcap.cpp: Remove redundant main()

--- windowcap.cpp
+++ windowcap.cpp
@@ -5,7 +5,7 @@
   Description:           Python interface.
                          Adapted from https://docs.python.org/3/extending/extending.html
   Creation Date:         2025-05-13
-  Modification Date:     2025-05-14
+  Modification Date:     2025-05-15
 
 */
 
@@ -68,50 +68,4 @@ static PyObject* windowcap_screenshot_window(PyObject* self, PyObject* args)
     delete buf;
 
     return PyTuple_Pack(3, PyLong_FromLong(width), PyLong_FromLong(height), bytes);
-}
-
-int main(int argc, char* argv[])
-{
-    PyStatus status;
-    PyConfig config;
-    PyConfig_InitPythonConfig(&config);
-    PyObject* pmodule;
-
-    // Add our module as a built-in, before Py_Initialize
-    if (PyImport_AppendInittab(MODULE_NAME, PyInit_windowcap) == -1) {
-        std::cerr << "Error: could not extend in-built modules table" << std::endl;
-
-        return 1;
-    }
-
-    // Pass argv[0] to the Python interpreter
-    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-
-    if (PyStatus_Exception(status)) {
-        goto exception;
-    }
-
-    // Initialize the Python interpreter.
-    status = Py_InitializeFromConfig(&config);
-
-    if (PyStatus_Exception(status)) {
-        goto exception;
-    }
-
-    PyConfig_Clear(&config);
-
-    // Import the module
-    pmodule = PyImport_ImportModule(MODULE_NAME);
-
-    if (!pmodule) {
-        PyErr_Print();
-        std::cerr << "Error: could not import module '" << MODULE_NAME << "'"
-                  << std::endl;
-    }
-
-    return 0;
-
-exception:
-    PyConfig_Clear(&config);
-    Py_ExitStatusException(status);
 }
