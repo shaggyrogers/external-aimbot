dc4529c42ba22d9dabab8cc2c9c99c1a41e778fd
shaggyrogers <shaggyrogers@not.real.net>
2025-05-21 12:39:54 +1000

    Move windowcap source to subdirectory

--- make-windowcap.sh
+++ make-windowcap.sh
@@ -1,1 +1,6 @@
-g++ windowcap_x11.cpp windowcap.cpp -std=c++17 -fPIC -shared -o windowcap.so `pkg-config --cflags --libs python3 opencv4 x11`
+g++ windowcap_src/windowcap_x11.cpp windowcap_src/windowcap.cpp \
+    -std=c++17 \
+    -fPIC \
+    -shared \
+    -o windowcap.so \
+    `pkg-config --cflags --libs python3 opencv4 x11`
